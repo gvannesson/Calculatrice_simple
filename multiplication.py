ab3c7645b543856f5ee0e91a6d3011a8e966ec49
Astambar <leuchartjason@gmail.com>
2024-10-23 11:56:38 +0200

    ajout addition.py

---
+++ multiplication.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def multiplication(number_1:int|float, number_2:int|float) -> int|float:
+    """_summary_
+
+    Args:
+        number_1 (int | float): _description_
+        number_2 (int | float): _description_
+
+    Returns:
+        int|float: _description_
+    """
+    return number_1 * number_2
