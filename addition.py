a09e75091e130a241b4c8fb4afefac314e7f2642
Astambar <leuchartjason@gmail.com>
2024-10-23 12:03:28 +0200

    addition verif int|float + implementation function

---
+++ addition.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+def addition(number_1:int|float,number_2:int|float) -> int|float:
+    if isinstance(number_1,int|float) | isinstance(number_2, int|float):
+        return (number_1 + number_2)
