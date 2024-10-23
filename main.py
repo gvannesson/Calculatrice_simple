37e885554cd8eb8214ce2539e87f7dcfa34fc81a
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2024-10-23 15:43:38 +0200

    création de modulo et modification de mainpy avec l'ajout de modulo et des exemples

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from addition import addition
 from division import division
-
-division(15/5)
 from soustraction import soustraction
+from mulplication import mulplication
+from modulo import le_modulo
+
+division(15,5)
+addition(15,5)
+soustraction(15,5)
+mulplication(15,5)
+le_modulo(15,5)
+
