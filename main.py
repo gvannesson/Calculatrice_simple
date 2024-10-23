34764eaca913929152ff0acad46bf6b5a18859d7
gvannesson <g.vannesson@gmail.com>
2024-10-23 15:24:52 +0200

    main avec puissance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mulplication import mulplication
 from addition import addition
 from division import division
 from soustraction import soustraction
+from exponentiation import puissance
 
 division(15/5)
 
