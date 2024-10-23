b4aea34411d23ae8706796b2ce5fe370942a1bd7
Astambar <leuchartjason@gmail.com>
2024-10-23 15:40:25 +0200

    add from logarithme in main and add if name in logarithme

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from mulplication import mulplication
 from addition import addition
 from division import division
 from soustraction import soustraction
-
+from logarithme import logarithme
 division(15/5)
 
