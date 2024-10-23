30e1db565b33c97c63afd376af4d9c0431320ef0
Astambar <leuchartjason@gmail.com>
2024-10-23 12:15:28 +0200

    addition add main test

--- addition.py
+++ addition.py
@@ -3,3 +3,11 @@
 def addition(number_1:int|float,number_2:int|float) -> int|float:
     if isinstance(number_1,int|float) and isinstance(number_2, int|float):
         return (number_1 + number_2)
+
+if __name__ == '__main__':
+    number_1 = 3
+    number_2 = 4
+    letter = "a"
+    print(addition(number_1, number_2))
+    print(addition(number_1, letter))
+    
